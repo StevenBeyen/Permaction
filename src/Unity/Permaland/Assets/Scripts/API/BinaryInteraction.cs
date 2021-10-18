eaff4a7c14df82ae8ea42a7929acd91e645ae796
Steven Beyen <steven.beyen@hotmail.be>
2021-10-18 14:24:10 +0200

    Unity: renamed to 'Permaland'.

---
+++ src/Unity/Permaland/Assets/Scripts/API/BinaryInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace API
+{
+    [System.Serializable]
+    public class BinaryInteraction
+    {
+        public int id;
+        public int element1_id;
+        public int element2_id;
+        public int interaction_level;
+        public string description;
+        
+        public BinaryInteraction()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return "id: " + id + ", element1_id: " + element1_id + ", element2_id: " + element2_id + ", interaction_level: " + interaction_level; 
+        }
+    }
+}
---
+++ src/Unity/Permaland/Assets/Scripts/API/BinaryInteractions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace API
+{
+    [System.Serializable]
+    public class BinaryInteractions : API
+    {
+        private const string BINARY_INTERACTIONS_URI = API_URI + "/binary_interactions";
+
+        public BinaryInteraction[] binary_interactions;
+
+        public BinaryInteractions()
+        {
+
+        }
+
+        public void BinaryInteractionsCallback(UnityWebRequest webRequest)
+        {
+            Debug.Log(webRequest.downloadHandler.text);
+            //this.binary_interactions = JsonUtility.FromJson<BinaryInteractions>(webRequest.downloadHandler.text).binary_interactions;
+        }
+
+        public string GetBinaryInteractionsURI()
+        {
+            return BINARY_INTERACTIONS_URI;
+        }
+
+        public override string ToString()
+        {
+            string reply = "[";
+            foreach (BinaryInteraction binary_interaction in binary_interactions)
+                reply += binary_interaction.ToString() + " ; \n";
+            reply += "]";
+            return reply;
+        }
+    }
+}
