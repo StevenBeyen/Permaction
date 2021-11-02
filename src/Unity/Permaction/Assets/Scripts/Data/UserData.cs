c3051f48763b337d3c1a8874e7b57fa7b573d197
Steven Beyen <steven.beyen@hotmail.be>
2021-11-02 10:43:48 +0100

    [Unity] Added comments to user data parameters.

--- src/Unity/Permaction/Assets/Scripts/Data/UserData.cs
+++ src/Unity/Permaction/Assets/Scripts/Data/UserData.cs
@@ -7,15 +7,19 @@ using Graphical;
 
 public static class UserData
 {
+    // Initialization
     public static MetaData meta_data = new MetaData();
     public static User user;
+    // Menu
     public static List<int> selectedElementIds;
     public static List<Element> selectedElements = new List<Element>();
+    // Terrain
     public static string[][] terrain_heightmap;
     public static bool terrain_loaded = false;
     public static PlacementReply reply;
     public static bool elements_loaded = false;
     public static GraphicalElement selectedElement = null;
+    // Additional data
     public static BinaryInteractions binaryInteractions = null;
     public static List<PhysicalElement> physicalElements = new List<PhysicalElement>();
 }
