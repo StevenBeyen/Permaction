ea11171dc6663e037d0c2654ebf44d74f28dbc72
Steven Beyen <steven.beyen@hotmail.be>
2022-01-05 12:20:57 +0100

    [Unity] First version of prefab enhancer, with test on aromatic plants.

---
+++ src/Unity/Permaction/Assets/Scripts/Graphical/PrefabEnhancer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabEnhancer : MonoBehaviour
+{
+    public GameObject element;
+    public float elementsPerSquareMeter = 1.0f;
+    public float scaleRange = 1.25f;
+    public float scaleOffset = 0.5f;
+    public Vector2 xBounds = new Vector2(-0.45f, 0.45f);
+    public Vector2 zBounds = new Vector2(-0.45f, 0.45f);
+
+    private System.Random random;
+    private Vector3 parentScale, parentPosition;
+    private Vector2 effectiveXBounds, effectiveZBounds;
+
+    void Start()
+    {
+        random = new System.Random();
+        GetParentData();
+        RenderElements();
+    }
+
+    private void GetParentData()
+    {
+        parentScale = gameObject.transform.localScale;
+        parentPosition = gameObject.transform.position;
+        if (gameObject.transform.localEulerAngles.y % 180 == 0)
+        {
+            effectiveXBounds = xBounds * parentScale.x;
+            effectiveZBounds = zBounds * parentScale.z;
+        } else
+        {
+            effectiveXBounds = xBounds * parentScale.z;
+            effectiveZBounds = zBounds * parentScale.x;
+        }
+    }
+
+    private void RenderElements()
+    {
+        int nb_elements = Mathf.RoundToInt((float) (0.9f + random.NextDouble() / 5.0f) * elementsPerSquareMeter * parentScale.x * parentScale.z);
+        float scale, x_pos, z_pos;
+        Vector3 position;
+        for (int i = 0; i < nb_elements; ++i)
+        {
+            // Instantiate in prefab's local scale before stretching.
+            scale = (float) random.NextDouble() * scaleRange + scaleOffset;
+            x_pos = (float) (parentPosition.x + (effectiveXBounds[0] + random.NextDouble() * (effectiveXBounds[1] - effectiveXBounds[0])));
+            z_pos = (float) (parentPosition.z + (effectiveZBounds[0] + random.NextDouble() * (effectiveZBounds[1] - effectiveZBounds[0])));
+            position = new Vector3(x_pos, parentPosition.y, z_pos);
+            GameObject instantiatedElement = Instantiate(element, position, Quaternion.identity);
+            instantiatedElement.transform.localScale = new Vector3(scale, scale, scale);
+            instantiatedElement.transform.Rotate(0, (float) (random.NextDouble() * 360), 0);
+        }
+    }
+}
