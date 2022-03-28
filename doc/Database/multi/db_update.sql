ca15d202eea16f2a85d3ff4f7d17f67bdb2331ab
Steven Beyen <steven.beyen@hotmail.be>
2022-03-28 09:32:20 +0200

    [Doc] Updated database and added new versions of the logo.

--- doc/Database/multi/db_update.sql
+++ doc/Database/multi/db_update.sql
@@ -295,3 +295,8 @@ update binary_interaction set interaction_level = 9 where id = 64;
 update category set physical_category = 0 where id = 5;
 update category set physical_category = 0 where id = 11;
 
+
+
+update binary_interaction set description = 'The feed forest provides food and shelter to the poultry.' where id = 17;
+update binary_interaction set description = 'La forêt nourricière sert de garde-manger et d’abri pour la volaille.' where id = 115;
+
