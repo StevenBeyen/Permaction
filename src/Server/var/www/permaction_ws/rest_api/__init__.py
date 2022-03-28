0b2ce8241c26b7d735452cfd98b84deb7837037e
Steven Beyen <steven.beyen@hotmail.be>
2022-03-28 19:18:16 +0200

    [Server] Attempt to solve CORS issue on server-side.

--- src/Server/var/www/permaction_ws/rest_api/__init__.py
+++ src/Server/var/www/permaction_ws/rest_api/__init__.py
@@ -6,6 +6,7 @@ from flask import Flask, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_paranoid import Paranoid
+from flask_cors import CORS
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -14,6 +15,7 @@ login_manager = LoginManager()
 def create_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
+    CORS(app)
     app.config.from_object('config.Config')
     db.init_app(app)
     login_manager.init_app(app)
